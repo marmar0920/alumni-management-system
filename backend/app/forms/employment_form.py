dc1d576f84d23857c3beac31968b124279446e7f
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:38:48 -0400

    Update employment_form.py
    add employment form

--- backend/app/forms/employment_form.py
+++ backend/app/forms/employment_form.py
@@ -1,1 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class EmploymentForm(FlaskForm):
+    companyName = StringField('Company Name', validators=[DataRequired(), Length(max=100)])
+    jobTitle = StringField('Job Title', validators=[DataRequired(), Length(max=100)])
+    startDate = DateField('Start Date', format='%Y-%m-%d', validators=[DataRequired()])
+    endDate = DateField('End Date', format='%Y-%m-%d')
+    currentYN = SelectField('Current?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
+    primaryYN = SelectField('Primary?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
+    submit = SubmitField('Save')
 
