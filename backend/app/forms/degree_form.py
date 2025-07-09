f5868a91b3440f4ca83f3f875c71fdd35c57cc2d
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:38:12 -0400

    Update degree_form.py
    add degree form

--- backend/app/forms/degree_form.py
+++ backend/app/forms/degree_form.py
@@ -1,1 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class DegreeForm(FlaskForm):
+    degreeType = StringField('Degree Type', validators=[DataRequired(), Length(max=50)])
+    institution = StringField('Institution', validators=[DataRequired(), Length(max=100)])
+    major = StringField('Major', validators=[DataRequired(), Length(max=100)])
+    conferredDate = DateField('Conferred Date', format='%Y-%m-%d', validators=[DataRequired()])
+    primaryYN = SelectField('Primary?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
+    submit = SubmitField('Save')
 
