304245b964df128db9e20650d7fab97287313096
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:40:04 -0400

    Update newsletter_form.py
    add newsletter form

--- backend/app/forms/newsletter_form.py
+++ backend/app/forms/newsletter_form.py
@@ -1,1 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, DateField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class NewsletterForm(FlaskForm):
+    subject = StringField('Subject', validators=[DataRequired(), Length(max=150)])
+    body = TextAreaField('Body', validators=[DataRequired()])
+    sendDate = DateField('Send Date', format='%Y-%m-%d')
+    submit = SubmitField('Save')
 
