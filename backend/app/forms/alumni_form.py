eaf7a84a3b8d5903315ed78b1afaabcc6edb4f38
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:37:05 -0400

    Update alumni_form.py
    add alumni form

--- backend/app/forms/alumni_form.py
+++ backend/app/forms/alumni_form.py
@@ -1,1 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+class AlumniForm(FlaskForm):
+    fName = StringField('First Name', validators=[DataRequired(), Length(max=20)])
+    lName = StringField('Last Name', validators=[DataRequired(), Length(max=20)])
+    email = StringField('Email', validators=[Email(), Length(max=50)])
+    phone = StringField('Phone', validators=[Length(max=10)])
+    DOB = DateField('Date of Birth', format='%Y-%m-%d')
+    gender = StringField('Gender', validators=[Length(max=1)])
+    ethnicity = StringField('Ethnicity', validators=[Length(max=1)])
+    website = StringField('Website', validators=[Length(max=100)])
+    submit = SubmitField('Save')
 
