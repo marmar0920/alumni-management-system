014e5e6b808ea817b62359d8b46d247f02378bc0
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:39:12 -0400

    Update donation_form.py
    add donation form

--- backend/app/forms/donation_form.py
+++ backend/app/forms/donation_form.py
@@ -1,1 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import DateField, DecimalField, StringField, TextAreaField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+class DonationForm(FlaskForm):
+    donationDate = DateField('Donation Date', format='%Y-%m-%d', validators=[DataRequired()])
+    amount = DecimalField('Amount', validators=[DataRequired()])
+    method = StringField('Method', validators=[DataRequired()])
+    recurringYN = SelectField('Recurring?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
+    notes = TextAreaField('Notes')
+    submit = SubmitField('Save')
 
