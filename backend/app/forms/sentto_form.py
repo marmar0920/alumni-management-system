b71816947720f0b34ced0ecee68f74ae7a753630
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:40:29 -0400

    Update sentto_form.py
    add send to form

--- backend/app/forms/sentto_form.py
+++ backend/app/forms/sentto_form.py
@@ -1,1 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, DateField, SubmitField
+from wtforms.validators import DataRequired
+
+class SenttoForm(FlaskForm):
+    newsletterID = SelectField('Newsletter', coerce=int, validators=[DataRequired()])
+    alumniID = SelectField('Alumnus', coerce=int, validators=[DataRequired()])
+    sentDate = DateField('Sent Date', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Save')
 
