166289ba7e25952a197f9483d2aa92aeb861d52c
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-08 23:48:00 -0400

    Update alumni.py
    add alumni blueprint with list and add routes

--- backend/app/routes/alumni.py
+++ backend/app/routes/alumni.py
@@ -1,1 +1,28 @@
+from flask import Blueprint, render_template, redirect, url_for, session, flash, request
+from ..models.alumni import Alumni
+from ..forms.alumni_form import AlumniForm
+from ..utils.db_connect import db
 
+alumni_bp = Blueprint('alumni', __name__, url_prefix='/alumni')
+
+@alumni_bp.route('/list')
+def list():
+    if not session.get('perms', {}).get('view'):
+        flash('Unauthorized', 'warning')
+        return redirect(url_for('auth.login'))
+    data = Alumni.query.all()
+    return render_template('alumni_list.html', alumni=data)
+
+@alumni_bp.route('/add', methods=['GET','POST'])
+def add():
+    if not session.get('perms', {}).get('insert'):
+        flash('Not allowed', 'warning')
+        return redirect(url_for('alumni.list'))
+    form = AlumniForm()
+    if form.validate_on_submit():
+        obj = Alumni(**{f: getattr(form, f).data for f in form.data if f != 'csrf_token'})
+        db.session.add(obj)
+        db.session.commit()
+        flash('Added successfully', 'success')
+        return redirect(url_for('alumni.list'))
+    return render_template('alumni_form.html', form=form)
