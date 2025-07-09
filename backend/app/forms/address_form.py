3045792b5e1bde9982457ad4effe327d63a462af
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:37:37 -0400

    Update address_form.py
    add address form

--- backend/app/forms/address_form.py
+++ backend/app/forms/address_form.py
@@ -1,1 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length
 
+class AddressForm(FlaskForm):
+    address = StringField('Street Address', validators=[DataRequired(), Length(max=50)])
+    city = StringField('City', validators=[DataRequired(), Length(max=50)])
+    state = StringField('State', validators=[DataRequired(), Length(min=2, max=2)])
+    zipCode = StringField('Zip Code', validators=[DataRequired(), Length(max=10)])
+    activeYN = SelectField('Active?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
+    primaryYN = SelectField('Primary?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
+    submit = SubmitField('Save')
