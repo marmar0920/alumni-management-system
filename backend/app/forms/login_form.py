f710331f3e8cc170955553ee3c56b31eaee9d549
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-09 10:36:35 -0400

    Update login_form.py
    add LoginForm

--- backend/app/forms/login_form.py
+++ backend/app/forms/login_form.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
+class LoginForm(FlaskForm):
+    UID = StringField('User ID', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
