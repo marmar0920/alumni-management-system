08d8b14646d24fd52aaebd53feb14339dbdb637a
marmar0920 <mmentor1@students.kennesaw.edu>
2025-07-10 19:58:02 -0400

    runs db create all to make sure all tables exsit

---
+++ setup_db.py
@@ -0,0 +1,24 @@
+from run import app
+from backend.utils.db_connect import db
+from backend.models.user import User
+
+with app.app_context():
+    print("Creating all database tables (if not exist)...")
+    db.create_all()
+
+    print("Checking for existing test user...")
+    existing_user = User.query.filter_by(UID='testuser').first()
+    if existing_user:
+        print("Test user already exists!")
+    else:
+        new_user = User(
+            UID='testuser',
+            password='testpass',
+            viewPriveledgeYN='Y',
+            insertPriveledgeYN='Y',
+            updatePriveledgeYN='Y',
+            deletePriveledgeYN='Y'
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print("Test user created successfully!")
