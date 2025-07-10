ad2f81590e2cb5c9fcaa20320f2105f6d869901a
marmar0920 <mmentor1@students.kennesaw.edu>
2025-07-10 18:13:03 -0400

    changed capitalzation

--- backend/app/forms/sentto_form.py
+++ backend/app/forms/sentto_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, DateField, SubmitField
 from wtforms.validators import DataRequired
 
-class SenttoForm(FlaskForm):
+class SentToForm(FlaskForm):
     newsletterID = SelectField('Newsletter', coerce=int, validators=[DataRequired()])
     alumniID = SelectField('Alumnus', coerce=int, validators=[DataRequired()])
     sentDate = DateField('Sent Date', format='%Y-%m-%d', validators=[DataRequired()])
