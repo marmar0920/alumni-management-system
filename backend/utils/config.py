4cb242f1155e043e0aa6f9d6ebe1c4f22d7560b8
marmar0920 <mmentor1@students.kennesaw.edu>
2025-07-10 18:18:17 -0400

    set bind for sql datbase uri

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-  SECRET_KEY = os.getenv('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'replace_this_with_a_random_secret'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///alumni.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
