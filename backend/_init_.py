24023ea550f8ee96b71dd0f16585d3a3516913a7
marmar0920 <mmentor1@students.kennesaw.edu>
2025-07-10 15:59:28 -0400

    fix _init_.py import for create_app

--- backend/_init_.py
+++ backend/_init_.py
@@ -14,10 +14,10 @@ from backend.app.routes.sentto_routes import sentto_bp
 
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-
+    # Register Blueprints here
     app.register_blueprint(auth_bp)
     app.register_blueprint(alumni_bp)
     app.register_blueprint(address_bp)
