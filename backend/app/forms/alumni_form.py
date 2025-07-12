d9a7354dfc9ee15a1231ae126ce239329a67d10a
Marsita <mmentor1@students.kennesaw.edu>
2025-07-12 18:04:59 -0400

    increase character limit for phone number in alumni form

--- backend/app/forms/alumni_form.py
+++ backend/app/forms/alumni_form.py
@@ -6,7 +6,7 @@ class AlumniForm(FlaskForm):
     fName = StringField('First Name', validators=[DataRequired(), Length(max=20)])
     lName = StringField('Last Name', validators=[DataRequired(), Length(max=20)])
     email = StringField('Email', validators=[Email(), Length(max=50)])
-    phone = StringField('Phone', validators=[Length(max=10)])
+    phone = StringField('Phone', validators=[Length(max=20)])
     DOB = DateField('Date of Birth', format='%Y-%m-%d')
     gender = StringField('Gender', validators=[Length(max=1)])
     ethnicity = StringField('Ethnicity', validators=[Length(max=1)])
