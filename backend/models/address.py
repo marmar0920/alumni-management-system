d099bc5a872b852aaca55382455cdbfab167e60a
Marsita <mmentor1@students.kennesaw.edu>
2025-07-12 20:33:04 -0400

    Fix address model field definitions and improve string representation

--- backend/models/address.py
+++ backend/models/address.py
@@ -1,17 +1,21 @@
-from backend.utils.db_connect import db
+ffrom backend.utils.db_connect import db
 
 class Address(db.Model):
     __tablename__ = 'address'
 
     addressID = db.Column(db.Integer, primary_key=True)
     alumniID = db.Column(db.Integer, db.ForeignKey('alumni.alumniID'))
-    address = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    state = db.Column(db.String(2))
-    zipCode = db.Column(db.String(10))
-    activeYN = db.Column(db.String(1))
-    primaryYN = db.Column(db.String(1))
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    zipCode = db.Column(db.String(10), nullable=False)
+    activeYN = db.Column(db.String(1), default='Y')
+    primaryYN = db.Column(db.String(1), default='N')
+
+    # alumni = relationship comes from Alumni.backref('alumnus')
 
     def __repr__(self):
+        return f'<Address {self.address}, {self.city}, {self.state}>'
+
         return f'<Address {self.address}>'
 
