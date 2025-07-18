0deb98df12597e82869ae81caf604b2980cfbb41
Marsita <mmentor1@students.kennesaw.edu>
2025-07-18 01:41:03 -0400

    fix: add conferred date field to degree form

--- backend/app/forms/degree_form.py
+++ backend/app/forms/degree_form.py
@@ -19,6 +19,7 @@ class DegreeForm(FlaskForm):
     major = StringField('Major', validators=[DataRequired(), Length(max=50)])
     minor = StringField('Minor', validators=[Length(max=50)])
     graduationDate = DateField('Graduation Date', format='%Y-%m-%d')
+    conferredDate = DateField('Conferred Date', format='%Y-%m-%d')
     institution = StringField('Institution', validators=[Length(max=100)])
     university = StringField('University', validators=[Length(max=100)])
     city = StringField('City', validators=[Length(max=50)])
