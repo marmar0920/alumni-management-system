38555bf6ed3dcd1210ccbe498241bf86874854e1
Marsita <mmentor1@students.kennesaw.edu>
2025-07-17 22:35:34 -0400

    add test route for login template rendering

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from backend import create_app
 
 app = create_app()
 
+@app.route("/_test_login_template")
+def _test_login_template():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
