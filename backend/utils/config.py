625fba7bb98d1276b2e71b64a80ae1cd1c7e48be
Marsita <mmentor1@students.kennesaw.edu>
2025-07-19 17:06:15 -0400

    fix: update database URI for improved connection settings

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -4,5 +4,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = 'replace_this_with_a_random_secret'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///alumni.db'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mardnie@localhost/alumnidb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
