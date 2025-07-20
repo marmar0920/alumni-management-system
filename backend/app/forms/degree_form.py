dd9c7cca25416c23ccbfb7c1b04316a35db88db1
marmar0920 <96851372+marmar0920@users.noreply.github.com>
2025-07-20 18:56:18 -0400

    Update degree_form.py

--- backend/app/forms/degree_form.py
+++ backend/app/forms/degree_form.py
@@ -20,7 +20,7 @@ class DegreeForm(FlaskForm):
     minor = StringField('Minor', validators=[Length(max=50)])
     graduationDate = DateField('Graduation Date', format='%Y-%m-%d')
     conferredDate = DateField('Conferred Date', format='%Y-%m-%d')
-    university = StringField('University', validators=[Length(max=100)])
+    instiution = StringField('Instiution', validators=[Length(max=100)])
     city = StringField('City', validators=[Length(max=50)])
     state = StringField('State', validators=[Length(max=2)])
     primaryYN = SelectField(
@@ -28,4 +28,4 @@ class DegreeForm(FlaskForm):
         choices=[('Y', 'Yes'), ('N', 'No')],
         validators=[DataRequired()]
     )
-    submit = SubmitField('Save')
+    submit = SubmitField('Save')#
