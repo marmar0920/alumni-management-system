440c1e59bc10acfb8e1cc999ec7b27caffec9a4c
Marsita <mmentor1@students.kennesaw.edu>
2025-07-20 18:14:54 -0400

    update employment form to make city, state, zip, startDate, and endDate fields optional

--- backend/app/forms/employment_form.py
+++ backend/app/forms/employment_form.py
@@ -1,20 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 class EmploymentForm(FlaskForm):
     alumniID = IntegerField('Alumni ID', validators=[DataRequired()])
     company = StringField('Company', validators=[DataRequired(), Length(max=100)])
-    city = StringField('City', validators=[Length(max=50)])
-    state = StringField('State', validators=[Length(max=2)])
-    zip = StringField('Zip Code', validators=[Length(max=10)])
+    city = StringField('City', validators=[Optional(), Length(max=50)])
+    state = StringField('State', validators=[Optional(), Length(max=2)])
+    zip = StringField('Zip Code', validators=[Optional(), Length(max=10)])
     jobTitle = StringField('Job Title', validators=[DataRequired(), Length(max=100)])
-    startDate = DateField('Start Date', format='%Y-%m-%d')
-    endDate = DateField('End Date', format='%Y-%m-%d')
+    startDate = DateField('Start Date', format='%Y-%m-%d', validators=[Optional()])
+    endDate = DateField('End Date', format='%Y-%m-%d', validators=[Optional()])
     currentYN = SelectField('Is Current?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
-    
     primaryYN = SelectField('Primary Employment?', choices=[('Y', 'Yes'), ('N', 'No')], validators=[DataRequired()])
-    
     submit = SubmitField('Save')
 
 
