33fb569802120ad8552ca4189d0ac2952dd0bbfc
Marsita <mmentor1@students.kennesaw.edu>
2025-07-21 21:26:33 -0400

    remove fname

--- backend/app/forms/degree_form.py
+++ backend/app/forms/degree_form.py
@@ -20,7 +20,7 @@ class DegreeForm(FlaskForm):
     minor = StringField('Minor', validators=[Length(max=50)])
     graduationDate = DateField('Graduation Date', format='%Y-%m-%d')
     conferredDate = DateField('Conferred Date', format='%Y-%m-%d')
-    institution = StringField('Institution', validators=[Length(max=100)])   # <-- MAKE SURE THIS IS PRESENT
+    institution = StringField('Institution', validators=[Length(max=100)]) 
     university = StringField('University', validators=[Length(max=100)])
     city = StringField('City', validators=[Length(max=50)])
     state = StringField('State', validators=[Length(max=2)])
